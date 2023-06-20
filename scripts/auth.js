7a2ac3f64aad18e572e3f3945f23ca93ac2581d3
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2023-06-19 18:01:56 -0600

    added admin function, code is not clean

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,6 +9,22 @@ const signUPBtn = document.getElementById('signUpBtn');
 const userEmail = document.getElementById('typeEmailX-2');
 const userPassword = document.getElementById('typePasswordX-2');
 const userName = document.getElementById('typeUser-2');
+const isChecked = document.getElementById('adminCheckbox')
+
+let isAdmin = false;
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  isChecked.addEventListener("change", () => {
+    if (isChecked.checked) {
+      isAdmin = true;
+      console.log('Admin: ', isAdmin)
+    } else {
+      isAdmin = false;
+      console.log('Admin: ', isAdmin)
+    }
+  });
+});
 
 //SIGNUP FUNCTION
 signUPBtn.addEventListener('click', (event) => {
@@ -20,13 +36,11 @@ signUPBtn.addEventListener('click', (event) => {
     .then(cred => {
 
       // const userInDB = ref(database, 'users/')
-      const user = cred.user;
-
-      const dateAndTime = new Date();
+      const user = cred.user
 
       set(ref(database, `users/ ${user.uid}`), {
         name: userNameValue,
-        date: dateAndTime,
+        admin: isAdmin
       })
 
     }).catch((error) => {
--- scripts/logedin.js
+++ scripts/logedin.js
@@ -9,6 +9,13 @@ const addIteamBtn = document.getElementById('addIteam')
 const itemInput = document.getElementById('cartIteam')
 const itemsInList = document.getElementById('listIteamsInDB')
 
+//ADMIN STUFF
+const isAdminCheckbox = document.getElementById('isAdminCheckbox');
+const updateAdminBtn = document.getElementById('updateAdminBtn');
+
+const clearList = () => {
+  itemsInList.innerHTML = ''
+}
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
@@ -20,11 +27,52 @@ onAuthStateChanged(auth, (user) => {
     const itemsInDB = ref(database, `usersItems/ ${uid}`)
 
 
+    updateAdminBtn.addEventListener('click', () => {
+      const isAdmin = isAdminCheckbox.checked;
+
+      // Update the "admin" property in the database
+      update(userInDB, {
+        admin: isAdmin
+      }).then(() => {
+        console.log('Admin status updated successfully');
+      }).catch((error) => {
+        console.log('Error updating admin status:', error);
+      });
+    });
+
+
     //READ USERS NAME
     onValue(userInDB, (snapshot) => {
       const data = snapshot.val();
       if (snapshot.exists()) {
-        userName.innerHTML = `${data.name}`;
+        const userData = snapshot.val();
+        const isAdmin = userData.admin;
+
+        userName.innerHTML = `${data.name} Admin: ${isAdmin}`;
+
+        if (isAdmin) {
+          // User is an admin
+          // Grant read and write access
+          // Perform the necessary actions for an admin user
+          //ADD SOMETHING TO REALTIME FIREBASE
+          document.body.style.backgroundColor = '#508bfc';
+
+
+          addIteamBtn.addEventListener('click', (event) => {
+            if (isAdmin) {
+              let itemInputValue = itemInput.value;
+              push(itemsInDB, itemInputValue)
+            } else {
+              console.log('not admin')
+            }
+          })
+        } else {
+          // User is not an admin
+          // Grant read access only
+          // Perform the necessary actions for a non-admin user
+          document.body.style.backgroundColor = 'lightgreen';
+          console.log('not admin')
+        }
       }
       else {
         userName.innerHTML = 'no user found';
@@ -32,14 +80,11 @@ onAuthStateChanged(auth, (user) => {
       }
     });
 
-    //ADD SOMETHING TO REALTIME FIREBASE
-    addIteamBtn.addEventListener('click', (event) => {
-      let itemInputValue = itemInput.value;
-      push(itemsInDB, itemInputValue)
-    })
+
 
     //ITEAMS IN REALTIME FIREBASE
     onValue(itemsInDB, (snapshot) => {
+      clearList()
       snapshot.forEach((childSnapshot) => {
         const childKey = childSnapshot.key;
         const childData = childSnapshot.val();
